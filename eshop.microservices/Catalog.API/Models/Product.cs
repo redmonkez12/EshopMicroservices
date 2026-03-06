9bee7f79101496a2855893f9f03dca75c129743d
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2026-03-06 07:11:39 +0100

    Add catalog API

---
+++ eshop.microservices/Catalog.API/Models/Product.cs
@@ -0,0 +1,11 @@
+namespace Catalog.API.Models;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public List<string> Category { get; set; } = [];
+    public string Description { get; set; } = null!;
+    public string ImageFile { get; set; } = null!;
+    public decimal Price { get; set; }
+}
